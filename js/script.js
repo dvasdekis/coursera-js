e31b34ed1cf63f9fe5106a8d648fb34107fc65b2
Dimitri Vasdekis <dimitrivasdekis@Dimitris-MacBook-Pro.local>
2019-04-04 13:38:33 +1100

    Did some work

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 var message = "In global scope";
 console.log("global: message = " + message);
+
+var a = function () {
+	var message = "inside a";
+	console.log("a: message = " + message);
+}
+
+a();
