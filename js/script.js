036b52cf775bf009f964d3345472f97f7e8d0bd5
Dimitri Vasdekis <dimitrivasdekis@Dimitris-MacBook-Pro.local>
2019-04-04 14:07:02 +1100

    Did some work

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ console.log(x);
 if (x == undefined) {
 	console.log("x is right now undefined");
 }
+
+x = 5;
+
+if (x == undefined) {
+	console.log("x is still undefined")
+}
+else {
+	console.log("x is defined!")
+}
