b479139d389f34a69caf7a2d43e26b3f68b5260d
Dimitri Vasdekis <dimitrivasdekis@Dimitris-MacBook-Pro.local>
2019-04-04 14:05:30 +1100

    Did some work

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-var message = "In global scope";
-console.log("global: message = " + message);
+var x;
+console.log(x);
 
-var a = function () {
-	var message = "inside a";
-	console.log("a: message = " + message);
-
-	function b () {
-		console.log("b: message = " + message);
-	}
-
-	b();
+if (x == undefined) {
+	console.log("x is right now undefined");
 }
-
-
-
-a();
