3991c951e433cfb39ea6ec28d5319976d76ccc3d
Dimitri Vasdekis <dimitrivasdekis@Dimitris-MacBook-Pro.local>
2019-04-04 14:24:52 +1100

    Did some work

--- js/lec43.js
+++ js/lec43.js
@@ -1,5 +1,9 @@
 // *** String concatentations
-var string = "Hello";
-string += " World";
-string = string + " World";
-console.log(string + "!");
+// var string = "Hello";
+// string += " World";
+// string = string + " World";
+// console.log(string + "!");
+
+
+// Math
+console.log((5 + 4) / 3);
