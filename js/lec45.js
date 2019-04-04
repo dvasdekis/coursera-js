53db5dc69159a5b5c050ba14e8c8f39802491354
Dimitri Vasdekis <dimitrivasdekis@Dimitris-MacBook-Pro.local>
2019-04-04 17:04:01 +1100

    Did some work

--- js/lec45.js
+++ js/lec45.js
@@ -2,9 +2,10 @@
 var company = new Object();
 
 company.name = "Facebook!";
+
 company.ceo = new Object();
 company.ceo.firstName = "Mark";
-
+company.ceo.favColor = "blue";
 
 company.log(company);
 
