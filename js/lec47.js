cf275f8962a69fc6a9778942dae2e0ed3ccf8041
Dimitri Vasdekis <dimitrivasdekis@Dimitris-MacBook-Pro.local>
2019-04-05 17:20:17 +1100

    Did some work

--- js/lec47.js
+++ js/lec47.js
@@ -1,13 +1,22 @@
 //Passing by reference vs by value
-
-var a = 7;
-var b = a;
 function printab (x, y) {
 	console.log("a: " + x);
 	console.log("b: " + y);
 }
-printab(a,b);
 
+console.log("Passing by value");
+var a = 7;
+var b = a;
+printab(a,b);
 b = 5
 console.log("After b update?");
 printab(a,b);
+
+console.log("Passing by reference");
+var c = {x: 7};
+var d = c;
+printab(c,d);
+
+d.x = 5;
+console.log("After b update?");
+printab(c,d);
