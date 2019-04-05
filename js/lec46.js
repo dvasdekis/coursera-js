35495f8bfa7c4386877678a1be08250a2ecda14a
Dimitri Vasdekis <dimitrivasdekis@Dimitris-MacBook-Pro.local>
2019-04-05 16:44:00 +1100

    Did some work

--- js/lec46.js
+++ js/lec46.js
@@ -11,3 +11,18 @@ console.log(multiply(5,3));
 multiply.version = "v1.6.9"
 console.log(multiply);
 console.log(multiply.version);
+
+//Function factory
+function makeMultiplier(multiplier) {
+	var myfunc = function(x) {;
+	return multiplier * x;
+	}
+
+	return myfunc;
+}
+
+var multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(10));
+
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(100));
